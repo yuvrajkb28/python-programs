d3e2568cfc50e7c653805bb220c4fc50a775a586
yuvrajkb28 <yuvarajkbhr64@gmail.com>
2025-07-19 11:08:33 +0530

    fift commit

--- 13.py
+++ 13.py
@@ -1,1 +1,6 @@
 # factorial of number
+num=int(input("enter the number "))
+result=1
+for i in range(1,num+1):
+    result=result*i
+print(result)
--- 14.py
+++ 14.py
@@ -1,1 +1,11 @@
-power of number
+# power of number
+base=int(input("enter the number "))
+exponet=int(input("enter the number"))
+result=1
+while exponet!=0:
+    result=result*base
+    exponet-=1
+print(result)
+
+
+
