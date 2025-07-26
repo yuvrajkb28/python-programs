f79c7e9fda487c5e71a7e99b82e6f3470fc5591a
yuvrajkb28 <yuvarajkbhr64@gmail.com>
2025-07-26 19:51:47 +0530

    29 30 commit

---
+++ 29.py
@@ -0,0 +1,5 @@
+# find the second largest number 
+l=[1,67,3,89,45,3,4]
+l.sort()
+print(l)
+print(l[-2])
---
+++ 30.py
@@ -0,0 +1,7 @@
+# min and max
+list=[89,23,56,80,12]
+print(list)
+list.sort()
+print(list)
+print(list[0])
+print(list[-1])
