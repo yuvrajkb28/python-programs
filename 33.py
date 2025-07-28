f3a44949da6cc170c46d1086670f1922c92432f8
yuvrajkb28 <yuvarajkbhr64@gmail.com>
2025-07-28 20:15:47 +0530

    33 34 commit

---
+++ 33.py
@@ -0,0 +1,6 @@
+# merging two array
+
+merg1=[12,45,67,43]
+merg2=[76,34,56,23]
+merg3=merg1+merg2
+print(merg3)
---
+++ 34.py
@@ -0,0 +1,8 @@
+# Remove duplicates from array
+
+list1=[12,35,35,45,45,89]
+a=set(list1)
+print(list1)
+print(a)
+b=list(a)
+print(b)
