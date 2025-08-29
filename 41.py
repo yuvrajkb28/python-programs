562e31d142d424893796af30bb2dda8af8262ce0
yuvrajkb28 <yuvarajkbhr64@gmail.com>
2025-08-29 23:13:14 +0530

    pattern finding

---
+++ 41.py
@@ -0,0 +1,11 @@
+# pattern finding
+# *
+# **
+# ***
+# ****
+# *****
+
+for i in range(0,5):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
---
+++ 44.py
@@ -0,0 +1,10 @@
+# 5 5 5 5 5
+# 4 4 4 4
+# 3 3 3
+# 2 2
+# 1
+
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print()
